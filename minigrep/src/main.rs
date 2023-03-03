4e94daf6c20e66bdf4ed40b88bbb997018b2ded8
gxt <15830407570@163.com>
2023-03-03 23:11:42 +0800

    first

---
+++ minigrep/src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use minigrep::Config;
+use std::process;
+
+fn main() {
+    // 读取命令行参数
+    let args :Vec<String> = env::args().collect();
+    // unwrap_or_else 产生一个闭包，打印错误信息，process 传入状态码退出程序
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        eprintln!("Problem parsing argumnets:{}",err);
+        process::exit(1);
+    });
+    if let Err(e) = minigrep::run(config){
+        eprintln!("Application error:{}",e);
+        process::exit(1)
+    }
+}
