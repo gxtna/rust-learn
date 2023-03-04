5e8970822f150196f5c17d2ffa3175358d17800d
gxt <15830407570@163.com>
2023-03-04 10:03:37 +0800

    使用迭代器和闭包优化该小程序

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -24,36 +24,50 @@ pub struct Config{
 }
 
 impl Config{
-    pub fn new(args:&[String])->Result<Config,&'static str>{
-        if args.len() < 3  {
+    pub fn new(mut arg: env::Args)->Result<Config,&'static str>{
+        if arg.len() < 3  {
             return Err("not enough argsuments");
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        // 因为需要的是第二个和第三个 ，所以第一个直接抛弃掉
+        arg.next();
+        let query = match arg.next(){
+            Some(arg) =>arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match arg.next(){
+            Some(arg) =>arg,
+            None => return Err("Didn't get a filename"),
+        };
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config{query,filename,case_sensitive})
     }
 }
 
 pub fn search<'a >(query:&str,contents: &'a str)->Vec<&'a str>{
-    let mut results =Vec::new();
-     for line in contents.lines(){
-        if line.contains(query) {
-            results.push(line)
-        } 
-     }
-     results
+    // let mut results =Vec::new();
+    //  for line in contents.lines(){
+    //     if line.contains(query) {
+    //         results.push(line)
+    //     } 
+    //  }
+    //  results
+
+    // 使用迭代器 和闭包减少代码 ，减少多余变量的维护
+     contents.lines()
+            .filter(|line| line.contains(query))
+            .collect()
 }
 
 pub fn search_case_insensitive<'a >(query:&str,contents: &'a str)->Vec<&'a str>{
-    let mut results =Vec::new();
-    let query =query.to_lowercase();
-     for line in contents.lines(){
-        if line.to_lowercase().contains(&query) {
-            results.push(line)
-        } 
-     }
-     results
+    // let mut results =Vec::new();
+    // let query =query.to_lowercase();
+    //  for line in contents.lines(){
+    //     if line.to_lowercase().contains(&query) {
+    //         results.push(line)
+    //     } 
+    //  }
+    //  results
+    contents.lines().filter(|line| line.to_lowercase().contains(&query.to_lowercase())).collect()
 }
 #[cfg(test)]
 mod tests{
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,9 +4,9 @@ use std::process;
 
 fn main() {
     // 读取命令行参数
-    let args :Vec<String> = env::args().collect();
+    // env::args 会产生一个迭代器
     // unwrap_or_else 产生一个闭包，打印错误信息，process 传入状态码退出程序
-    let config = Config::new(&args).unwrap_or_else(|err|{
+    let config = Config::new(env::args()).unwrap_or_else(|err|{
         eprintln!("Problem parsing argumnets:{}",err);
         process::exit(1);
     });
